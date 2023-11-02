5c5fac5bd8a0d3e1119ad4319cd3a1c4cbd4273d
Helen <helendianaf@gmail.com>
2023-11-02 14:47:46 -0300

    update exercicios

---
+++ src/tarefa_calculo_media/CalculoMedia.java
@@ -0,0 +1,13 @@
+package tarefa_calculo_media;
+
+public class CalculoMedia {
+    public static void main(String[] args) {
+        float notaUm = 8.5F;
+        float notaDois = 9;
+        float notaTres = 7.5F;
+        float notaQuatro = 10;
+        float media = (notaUm + notaDois + notaTres + notaQuatro)/4;
+
+        System.out.println("Sua média é: " + media);
+    }
+}
